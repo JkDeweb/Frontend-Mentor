21a7d427afd219edaa4236b4d0e37f90ee9a8447
Jack'er_ <104057685+JkDeweb@users.noreply.github.com>
2022-08-05 18:34:18 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const darkMode = document.querySelector('#img');
-darkMode.addEventListener('mouseover', () => {
-    document.body.classList.toggle('dark');
-});
-darkMode.addEventListener('mouseout', () => {
-    document.body.classList.toggle('dark');
-});
